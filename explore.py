a49d06e7a3f0809a73d7e30e492740f31c761fe8
scneeman <30477959+scneeman@users.noreply.github.com>
2023-12-27 20:35:37 -0600

    Add files via upload

---
+++ explore.py
@@ -0,0 +1,14 @@
+
+print("\n--- we can access part of a list (or string) ---")
+print("--- this is referred to as a 'slice' ---")
+words = 'Forescore and seven years ago...'
+nums = [10, 11, 12, 13, 14]
+
+print(words[0:3])
+print(nums[0:3])
+
+print(words[:2])
+print(nums[:2])
+
+print(words[2:])
+print(nums[2:])
---
+++ try1.py
@@ -0,0 +1,38 @@
+import random
+
+print("\n\n-------------------------------------")
+print(" --- slicing ---")
+nums = [random.randint(10,100) for i in range(15)]
+print("the list:", nums)
+# experiment with slicing
+
+
+# loop through a slice
+
+
+
+# 'copy' a list using assignment
+# (remember, this is actually a pointer to the same memory, not a copy of the data)
+
+
+# copy using list comprehension
+
+
+
+
+# copy a list using slicing
+
+
+
+# copy using .copy() (probably skip)
+
+
+
+print("\n\n-------------------------------------")
+print(" --- tuples ---")
+# ask for 3 numbers, create a tuple (r,g,b)
+# calculate the average of the values
+# loop through and print
+# try to change an item using its index
+# change the tuple
+
