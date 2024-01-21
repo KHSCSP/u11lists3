508e5fe676635578d6da5e7171f8d7e4a508e6a6
heyscottn <heyscottn@gmail.com>
2024-01-21 10:47:19 -0600

    sn

--- try1.py
+++ try1.py
@@ -5,22 +5,34 @@ print(" --- slicing ---")
 nums = [random.randint(10,100) for i in range(15)]
 print("the list:", nums)
 # experiment with slicing
+print("a few near the beginning:")
+print("a few near the middle:")
+print("a few near the end:")
 
 
 # loop through a slice
-
+print("\n\nlooping through a slice:")
 
 
 # 'copy' a list using assignment
 # (remember, this is actually a pointer to the same memory, not a copy of the data)
+print("\n\n'copying' a list using assignment:")
+
+print("changing one list changes both!")
+
+
 
 
 # copy using list comprehension
+print("\n\ncopy using comprehension:")
+print("this copies each item into a new list")
 
 
 
 
 # copy a list using slicing
+print("\n\ncopying usoing slicing")
+print("this copies each item into a new list")
 
 
 
