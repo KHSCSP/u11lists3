281c576c0d8b88d70753ada8fac767a948c2b5ae
heyscottn <heyscottn@gmail.com>
2024-01-21 10:12:00 -0600

    edits

--- apply.py
+++ apply.py
@@ -1,18 +1,23 @@
 print("\n---- part 1, 20 items ----")
-# create any list with at least 20 items
-# print the message "the first 3 items are:" then use slicing to print the first 3 items
-# print the message "3 items near the middle are:" then use slicing to display those items
-# print the message "the last 3 items are:" then display those items
-
+# 1) create any list with at least 20 items
+# 2) print the message "the first 3 items are:" then use slicing to print the first 3 items
+# 3) print the message "3 items near the middle are:" then use slicing to display those items
+# 4) print the message "the last 3 items are:" then display those items
+print("the first 3 items are:")
+print("the last 3 items are:")
 
 
 
 print("\n\n---- part 2, FYI ----")
 '''
-In the United States of America, telephone numbers within an area code consist of seven digits. 
-The prefix number is the first three digits and the line number is the last four digits. 
-Traditionally, the prefix number 555 has been used to provide directory information and assistance.
-For this problem, write a program that determines if  a supplied seven-digit telephone number should be routed to the directory information operator, that is, the prefix number is 555.
+In the United States of America, telephone numbers 
+within an area code consist of seven digits. 
+The prefix number is the first three digits and 
+the line number is the last four digits. 
+Traditionally, the prefix number 555 has been used
+to provide directory information and assistance.
+For this problem, write a program that determines if
+ a supplied seven-digit telephone number should be routed to the directory information operator, that is, the prefix number is 555.
 '''
 
 
@@ -22,8 +27,10 @@ For this problem, write a program that determines if  a supplied seven-digit tel
 
 print("\n\n---- part 3, Finding an A ----")
 '''
-In this problem, you are given a single string s that is guaranteed to contain the letter a.
-You should output the suffix of s that begins with the first occurrence of the letter a.
+In this problem, you are given a single string s 
+that is guaranteed to contain the letter a.
+You should output the suffix of s that begins with 
+the first occurrence of the letter a.
  '''
 
 
@@ -32,11 +39,18 @@ You should output the suffix of s that begins with the first occurrence of the l
 
 print("\n\n---- part 4, Autori ----")
 '''
-Great scientific discoveries are often named by the last names of scientists that made them. For example, the most popular asymmetric cryptography system, RSA was discovered by Rivest, Shamir and Adleman. Another notable example is the Knuth-Morris-Pratt algorithm, named by Knuth, Morris and Pratt.
-
-Scientific papers reference earlier works a lot and it’s not uncommon for one document to use two different naming conventions: the short variation (e.g. KMP) using only the first letters of authors last names and the long variation (e.g. Knuth-Morris-Pratt) using complete last names separated by hyphens.
-
-We find mixing two conventions in one paper to be aesthetically unpleasing and would like you to write a program that will transform long variations into short.
+Great scientific discoveries are often named 
+by the last names of scientists that made them. 
+For example, the most popular asymmetric cryptography system, RSA was discovered by Rivest, Shamir and Adleman. Another notable example is the Knuth-Morris-Pratt algorithm, named by Knuth, Morris and Pratt.
+Scientific papers reference earlier works a lot 
+and it’s not uncommon for one document to use 
+two different naming conventions: 
+the short variation (e.g. KMP) using only the first letters of authors last names 
+and the long variation (e.g. Knuth-Morris-Pratt) using complete last names separated by hyphens.
+We find mixing two conventions in one paper to be 
+aesthetically unpleasing and would like you to write
+a program that will transform long variations 
+into short.
 '''
 
 
