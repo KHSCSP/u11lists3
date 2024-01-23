7f274a2804ef1766a6e123435557a1f61942c834
heyscottn <heyscottn@gmail.com>
2024-01-23 12:16:41 -0600

    mm

--- explore.py
+++ explore.py
@@ -3,7 +3,7 @@ print("--- this is referred to as a 'slice' ---")
 words = 'Forescore and seven years ago...'
 nums = [10, 11, 12, 13, 14]
 
-print("indces 0:3")
+print("indices 0:3")
 print(words[0:3])
 print(nums[0:3])
 
